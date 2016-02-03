157eb464427fb323fa6b2acb1ec257ba26388d38
block7 <block7@b339-computer-lab-62.local>
2016-02-03 11:43:04 -0500

    Initial Commit

---
+++ RowingRouter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RowingRouter
+//
+//  Created by block7 on 2/3/16.
+//  Copyright © 2016 Calvin Terpstra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RowingRouterTests/RowingRouterTests.swift
@@ -0,0 +1,36 @@
+//
+//  RowingRouterTests.swift
+//  RowingRouterTests
+//
+//  Created by block7 on 2/3/16.
+//  Copyright © 2016 Calvin Terpstra. All rights reserved.
+//
+
+import XCTest
+@testable import RowingRouter
+
+class RowingRouterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
